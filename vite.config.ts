892a4c1917663d291983d5edff3dbb20210888f3
JayJay-rgb <pjayyyyyyy5@gmail.com>
2026-02-11 22:57:42 +0100

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+// @ts-ignore
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+// @ts-ignore
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
